0f82c10052c981d6ee4c1c752c19019a7785cbfd
Shamin Asfaq <shamin.asfaq@gmail.com>
2021-10-24 01:26:06 +0600

    Initial commit

---
+++ src/main/java/com/example/springbootldap/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.springbootldap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String index() {
+        return "Home Page";
+    }
+}
